c31b840aa5fa03a36e89a538b518d76ddc1eee96
Maharajan00 <74131515+Maharajan00@users.noreply.github.com>
2020-11-08 14:01:06 +0530

    Add files via upload

---
+++ MaharajanK_assg1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#1
+b.) RAINBOW
+
+
+# In[4]:
+
+
+#2
+print('LETS UPGRADE')
+
+
+# In[5]:
+
+
+#3
+cp=int(input("enter the cost price:"))
+sp=int(input("enter the selling price:"))
+if cp<sp:
+   print("PROFIT")
+elif cp>sp:
+     print("LOSS")
+else:
+    
+    print("NEITHER")
+
+
+# In[3]:
+
+
+#4
+ind=int(input("Indian Money:"))
+e=ind*80
+print("EURO=",e)
+
+
+# In[ ]:
+
+
+
+
